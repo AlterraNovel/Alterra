7c106b981a163de3ece62f98351d62ba636f5e33
Kiseki <32672889+Kiseki-0@users.noreply.github.com>
2025-02-27 09:14:55 -0300

    Atualizacao Final
    Atualizacao Final

---
+++ app/MapaAncestral/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+    return (
+      <div className="no-scrollbar overflow-y-auto ">
+        <main className="h-[95vh]">
+          <iframe
+            className="w-full h-full bg-[rgb(40,84,124)] p-0"
+            src="https://embed.figma.com/proto/PqePGLjBtxXsaufkek5ZBC/Mu---Mapa-Alterra-(Copy)?node-
+id=467-6&p=f&scaling=scale-down-width&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=467%3A6&embed-host=share&hide-ui=1&hide_ui=1"
+          />
+        </main>
+      </div>
+    );
+  }
+  
+  
